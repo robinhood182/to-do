e8cacc6f2878e0841d4c224bfdd45ad493916e3c
Robyn Navarro <rnavarro@c02xtbcgjg5j.redventures.net>
2019-04-04 16:22:26 -0400

    TodoList component taking props!

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <TodoList/>
+    <TodoList v-bind:todos="todos"/>
   </div>
 </template>
 
@@ -12,17 +12,35 @@ export default {
   name: 'App',
   components: {
     TodoList
+  },
+
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      },
+      {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      },
+      {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      },
+      {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
