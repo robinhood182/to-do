ce9cb690dfcbe5a8904151d4d1ae8a50005c44f2
Robyn Navarro <rnavarro@c02xtbcgjg5j.redventures.net>
2019-04-05 09:13:52 -0400

    refactored todo to be its own component

---
+++ todo-app/src/components/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="ui centered card">
+            <div class="content">
+                <div class="header">
+                    {{ todo.title }}
+                </div>
+                <div class="meta">
+                    {{ todo.project }}
+                </div>
+                <div class="extra-content">
+                    <span class="right floated edit icon">
+                        <i class="edit icon"></i>
+                    </span>
+                </div>
+            </div>
+            <div class="ui bottom attached green basic button" v-show="todo.done">
+                Completed
+            </div>
+            <div class="ui bottom attached red basic button" v-show="!todo.done">
+                Not yet completed
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    props: ['todo']
+};
+</script>
+
