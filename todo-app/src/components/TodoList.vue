38255ab5534d1162e405d6df9fe5712a4c3e9c9c
Robyn Navarro <rnavarro@c02xtbcgjg5j.redventures.net>
2019-04-05 11:16:30 -0400

    added delete functionality

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Completed tasks: {{ todos.filter(todo => todo.done === true).length}}</p>
         <p>Pending tasks: {{ todos.filter(todo => todo.done === false).length}}</p>
-        <todo v-for="todo in todos" :key="todo.id" :todo="todo"></todo>
+        <todo v-for="todo in todos" :key="todo.id" :todo="todo" v-on:delete-todo="deleteTodo"></todo>
     </div>
 </template>
 
@@ -13,6 +13,12 @@ export default {
     props: ['todos'],
     components: {
         Todo
+    },
+    methods: {
+        deleteTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        }
     }
 };
 </script>
